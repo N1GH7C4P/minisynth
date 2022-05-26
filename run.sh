faa26570c7a79ec1f77b9f2db790a3da876acc77
Caruychen <caruychen@gmail.com>
2022-05-26 14:08:41 +0300

    reading file lines

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+cargo build
 ./target/debug/minisynth
 ./target/debug/minisynth file1 file2
 ./target/debug/minisynth examples/Simple_Tune.synth
+./target/debug/minisynth examples/Prelude.synth
--- src/bin/minisynth.rs
+++ src/bin/minisynth.rs
@@ -1,4 +1,4 @@
-use rodio::{OutputStream};
+use rodio::{OutputStream, OutputStreamHandle};
 use minisynth::track::Track;
 use std::env;
 use std::fs;
@@ -12,29 +12,48 @@ fn stream(mut tracks: Vec<Track>, tempo: u32) {
 	}
 }
 
+fn get_desc(filename: &String) -> Vec<String> {
+	let contents = fs::read_to_string(&filename).expect("Something went wrong reading the file");
+	let mut desc: Vec<String> = contents.as_str().split('\n').map(str::to_string).collect();
+	for index in (0..desc.len()).rev() {
+		if desc[index].len() == 0 {
+			desc.remove(index);
+			continue;
+		}
+		if desc[index].chars().nth(0).unwrap() == '#' {
+			desc.remove(index);
+		}
+	}
+	desc
+}
+
+fn get_tempo(desc: &Vec<String>) -> u32 {
+	let tempo_str: Vec<&str> = desc[0].as_str().split(' ').collect();
+	tempo_str[1].parse().unwrap()
+}
+
+fn set_tracks(tracks: &mut Vec<Track>, desc: &Vec<String>, stream_handle: &OutputStreamHandle) {
+	let tmp_arr: Vec<&str> = desc[1].as_str().split(' ').collect();
+	let instruments: Vec<&str> = tmp_arr[1].split(',').collect();
+	for instrument in instruments.iter() {
+		tracks.push(Track::new(&stream_handle));
+	}
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2
-    {
-	println!("Usage: ./minisynth file");
-	std::process::exit(1);
-    }
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let tempo = 160;
-    let mut tracks : Vec<Track> = Vec::new();
-    tracks.push(Track::new(&stream_handle));
-    tracks.push(Track::new(&stream_handle));
-    tracks[0].add_note(261.63);
-    tracks[0].add_note(293.66);
-    tracks[0].add_note(329.63);
-    tracks[0].add_note(349.23);
-    tracks[0].add_note(329.63);
-    tracks[0].add_note(293.66);
-    tracks[1].add_note(65.42);
-    tracks[1].add_note(65.42);
-    tracks[1].add_note(65.42);
-    tracks[1].add_note(200.42);
-    tracks[1].add_note(200.42);
-    tracks[1].add_note(200.42);
-    stream(tracks, tempo);
+	let args: Vec<String> = env::args().collect();
+	if args.len() != 2
+	{
+	    println!("Usage: ./minisynth file");
+	    std::process::exit(1);
+	}
+	let mut tracks : Vec<Track> = Vec::new();
+	let desc: Vec<String> = get_desc(&args[1]);
+	let tempo = get_tempo(&desc);
+
+	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+	println!("len {}", tracks.len());
+	set_tracks(&mut tracks, &desc, &stream_handle);
+	println!("len {}", tracks.len());
+//	stream(tracks, tempo);
 }
