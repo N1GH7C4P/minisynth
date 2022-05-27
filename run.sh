bd3411edd9771b73d6673ac10833707a94919a3c
Caruy Chen <cchen@student.hive.fi>
2022-05-27 11:41:01 +0300

    plays cantina band

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cp ./target/release/minisynth ./
 #./target/release/minisynth examples/Prelude.synth
 #./target/release/minisynth examples/Imperial_March.synth
 #./target/release/minisynth examples/Super_Mario.synth
-#./target/release/minisynth examples/Cantina_Band.synth
+./target/release/minisynth examples/Cantina_Band.synth
 #./target/release/minisynth examples/Toccata.synth
-./target/release/minisynth examples/Nightcall.synth
+#./target/release/minisynth examples/Nightcall.synth
 #./target/release/minisynth examples/test.synth
