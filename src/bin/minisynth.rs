1520c4621a924e29a82a3336ab6d63ccba1a83b8
N1GH7C4P <kimmo.polojarvi@gmail.com>
2022-05-27 15:38:56 +0300

    cant read filenames for some reason

--- src/bin/minisynth.rs
+++ src/bin/minisynth.rs
@@ -3,6 +3,12 @@ use minisynth::track::Track;
 use minisynth::parser::parser;
 use minisynth::keymap::Keymap;
 use std::env;
+use std::io::{Error, ErrorKind};
+use std::fs::{self};
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::prelude::*;
+use std::{thread, time};
 
 fn stream(mut tracks: Vec<Track>) {
 	for t in 0..tracks.len() {
@@ -29,6 +35,34 @@ fn main() {
 	let tempo = parser::get_tempo(&desc);
 	let mut tracks = parser::get_tracks(&desc, &stream_handle);
 	parser::set_notes(&mut tracks, &desc, tempo, keymap.get());
+	
+	let mut contents: Vec<String> = Vec::new();
+	let filenames: Vec<PathBuf> = match fs::read_dir("../../assets/ascii") {
+        Err(e) if e.kind() == ErrorKind::NotFound => Vec::new(),
+        Err(e) => panic!("Unexpected Error! {:?}", e),
+        Ok(entries) => entries.filter_map(|e| e.ok())
+            .map(|e| e.path())
+            .collect()
+    };
+
+	let nb_files: usize = filenames.len();
+
+	for n in 0..nb_files
+	{
+		let mut f = File::open(&filenames[n]).expect("file not found");
+		f.read_to_string(&mut contents[n])
+			.expect("something went wrong reading the file");
+	}
+	thread::spawn(move || {
+		let frame_time = time::Duration::from_millis(200);
+		loop{
+			for n in 0..nb_files
+			{
+				println!("\n{}", contents[n]);
+				thread::sleep(frame_time);
+			}
+		}
+    });
 	println!("And begin!");
-	stream(tracks);
+	stream(tracks);	
 }
